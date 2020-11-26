4298b14afb9cb89fb5602cec0b554ef11c022ad6
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2020-11-26 20:11:57 +0500

    updated pakage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import 'semantic-ui-css/semantic.min.css';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import middleware from './middleware';
+import rootReducer from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, middleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+
+    <App />
+
+  </Provider>,
+  document.getElementById('root')
+);

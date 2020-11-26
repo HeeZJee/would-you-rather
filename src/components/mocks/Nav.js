3243623bd6f010a8f11b58cffc61ad8309c8d61c
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2020-11-26 18:15:24 +0500

    build app framework

--- src/components/mocks/Nav.js
+++ src/components/mocks/Nav.js
@@ -1,156 +1,132 @@
 import React, { Component, Fragment } from 'react';
+import { withRouter, NavLink } from 'react-router-dom';
 import {
-    Menu,
-    Responsive,
-    Image,
-    Grid,
-    Button,
-    Container
+  Menu,
+  Responsive,
+  Image,
+  Grid,
+  Button,
+  Container
 } from 'semantic-ui-react';
 
-export class Nav extends Component {
-    state = { activeItem: 'home' };
+class Nav extends Component {
+  handleLogout = e => {
+    e.preventDefault();
+    this.props.onLogout();
+  };
 
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
-
-    render() {
-        const { activeItem } = this.state;
-        return (
-            <Container>
-                <Responsive as={Menu} minWidth={651} pointing secondary>
-                    <Menu.Item
-                        name="home"
-                        active={activeItem === 'home'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Item
-                        name="new poll"
-                        active={activeItem === 'new poll'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Item
-                        name="leader board"
-                        active={activeItem === 'leader board'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Menu position="right">
-                        <Menu.Item>
-                            <span>
-                                <Image
-                                    src="images/avatars/fox.png"
-                                    avatar
-                                    spaced="right"
-                                    verticalAlign="bottom"
-                                />
+  render() {
+    return (
+      <Container>
+        <Responsive as={Menu} minWidth={651} pointing secondary>
+          <Menu.Item name="home" as={NavLink} to="/" exact />
+          <Menu.Item name="new poll" as={NavLink} to="/add" />
+          <Menu.Item name="leader board" as={NavLink} to="/leaderboard" />
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <span>
+                <Image
+                  src="/images/avatars/fox.png"
+                  avatar
+                  spaced="right"
+                  verticalAlign="bottom"
+                />
                 James Priest
               </span>
-                        </Menu.Item>
-                        <Menu.Item>
-                            <Button
-                                content="Logout"
-                                labelPosition="right"
-                                basic
-                                compact
-                                icon="log out"
-                                size="mini"
-                            />
-                        </Menu.Item>
-                    </Menu.Menu>
-                </Responsive>
-                <Responsive as={Fragment} minWidth={375} maxWidth={650}>
-                    <Grid columns={2} padded="vertically">
-                        <Grid.Row>
-                            <Grid.Column>
-                                <Image
-                                    src="images/avatars/fox.png"
-                                    avatar
-                                    spaced="right"
-                                    verticalAlign="bottom"
-                                />
+            </Menu.Item>
+            <Menu.Item>
+              <Button
+                content="Logout"
+                labelPosition="right"
+                basic
+                compact
+                icon="log out"
+                size="mini"
+                onClick={this.handleLogout}
+              />
+            </Menu.Item>
+          </Menu.Menu>
+        </Responsive>
+        <Responsive as={Fragment} minWidth={375} maxWidth={650}>
+          <Grid columns={2} padded="vertically">
+            <Grid.Row>
+              <Grid.Column>
+                <Image
+                  src="/images/avatars/fox.png"
+                  avatar
+                  spaced="right"
+                  verticalAlign="bottom"
+                />
                 James Priest
               </Grid.Column>
-                            <Grid.Column verticalAlign="bottom" textAlign="right">
-                                <Button
-                                    content="Logout"
-                                    labelPosition="right"
-                                    basic
-                                    compact
-                                    icon="log out"
-                                    size="mini"
-                                />
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <Grid.Column width={16}>
-                                <Menu pointing secondary widths={3}>
-                                    <Menu.Item
-                                        name="home"
-                                        active={activeItem === 'home'}
-                                        onClick={this.handleItemClick}
-                                    />
-                                    <Menu.Item
-                                        name="new poll"
-                                        active={activeItem === 'new poll'}
-                                        onClick={this.handleItemClick}
-                                    />
-                                    <Menu.Item
-                                        name="leader board"
-                                        active={activeItem === 'leader board'}
-                                        onClick={this.handleItemClick}
-                                    />
-                                </Menu>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Responsive>
-                <Responsive as={Fragment} maxWidth={374}>
-                    <Grid padded="vertically" columns={1}>
-                        <Grid.Row>
-                            <Grid.Column>
-                                <Image
-                                    src="images/avatars/fox.png"
-                                    avatar
-                                    spaced="right"
-                                    verticalAlign="bottom"
-                                />
+              <Grid.Column verticalAlign="bottom" textAlign="right">
+                <Button
+                  content="Logout"
+                  labelPosition="right"
+                  basic
+                  compact
+                  icon="log out"
+                  size="mini"
+                  onClick={this.handleLogout}
+                />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={16}>
+                <Menu pointing secondary widths={3}>
+                  <Menu.Item name="home" as={NavLink} to="/" exact />
+                  <Menu.Item name="new poll" as={NavLink} to="/add" />
+                  <Menu.Item
+                    name="leader board"
+                    as={NavLink}
+                    to="/leaderboard"
+                  />
+                </Menu>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Responsive>
+        <Responsive as={Fragment} maxWidth={374}>
+          <Grid padded="vertically" columns={1}>
+            <Grid.Row>
+              <Grid.Column>
+                <Image
+                  src="/images/avatars/fox.png"
+                  avatar
+                  spaced="right"
+                  verticalAlign="bottom"
+                />
                 James Priest
                 <Button
-                                    content="Logout"
-                                    labelPosition="right"
-                                    basic
-                                    compact
-                                    icon="log out"
-                                    size="mini"
-                                    floated="right"
-                                />
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <Grid.Column>
-                                <Menu pointing secondary widths={3}>
-                                    <Menu.Item
-                                        name="home"
-                                        active={activeItem === 'home'}
-                                        onClick={this.handleItemClick}
-                                    />
-                                    <Menu.Item
-                                        name="new poll"
-                                        active={activeItem === 'new poll'}
-                                        onClick={this.handleItemClick}
-                                    />
-                                    <Menu.Item
-                                        name="leader board"
-                                        active={activeItem === 'leader board'}
-                                        onClick={this.handleItemClick}
-                                    />
-                                </Menu>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Responsive>
-            </Container>
-        );
-    }
+                  content="Logout"
+                  labelPosition="right"
+                  basic
+                  compact
+                  icon="log out"
+                  size="mini"
+                  floated="right"
+                  onClick={this.handleLogout}
+                />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <Menu pointing secondary widths={3}>
+                  <Menu.Item name="home" as={NavLink} to="/" exact />
+                  <Menu.Item name="new poll" as={NavLink} to="/add" />
+                  <Menu.Item
+                    name="leader board"
+                    as={NavLink}
+                    to="/leaderboard"
+                  />
+                </Menu>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Responsive>
+      </Container>
+    );
+  }
 }
 
-export default Nav;
+export default withRouter(Nav);
